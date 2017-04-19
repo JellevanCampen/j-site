706a9ccb1b352cf9eb718a3540b12207b2ae6d36
Jelle van Campen <jellevcampen@gmail.com>
2017-04-19 21:01:46 +0200

    Clean up the repository (pt. 2): remove files that should not be tracked anymore.

--- _old/scripts/general.js
+++
@@ -1,5 +0,0 @@
-// Set an up-to-date copyright notice
-$(document).ready(function() {
-  var d = new Date();
-  $("#copyright-notice").html("Copyright &#9400; " + d.getFullYear() + " Jelle van Campen");
-});
