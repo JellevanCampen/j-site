358c0bd76f5e4834d219c0edfe8c9f9665477259
Jelle van Campen <jellevcampen@gmail.com>
2017-05-05 16:03:27 +0200

    Add navigation drawer that opens on pressing the menu button in the navigation bar.

--- user/themes/j-theme/js/theme.js
+++ user/themes/j-theme/js/theme.js
@@ -1,5 +1,19 @@
-// Set an up-to-date copyright notice
+// Update copyright date
 $(document).ready(function() {
   var d = new Date();
   $("#copyright-notice").html("Copyright &#9400; " + d.getFullYear() + " Jelle van Campen");
+});
+
+// Hook up collapsed navigation drawer
+var drawerEl = document.querySelector('.mdc-temporary-drawer');
+var MDCTemporaryDrawer = mdc.drawer.MDCTemporaryDrawer;
+var drawer = new MDCTemporaryDrawer(drawerEl);
+document.querySelector('.page-navigation-collapsed').addEventListener('click', function() {
+  drawer.open = true;
+});
+drawerEl.addEventListener('MDCTemporaryDrawer:open', function() {
+  console.log('Received MDCTemporaryDrawer:open');
+});
+drawerEl.addEventListener('MDCTemporaryDrawer:close', function() {
+  console.log('Received MDCTemporaryDrawer:close');
 });
